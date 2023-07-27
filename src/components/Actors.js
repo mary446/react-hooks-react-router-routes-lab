86c83b329735cc07a77ae8d9fbab9d67208b83a3
mary446 <135317707+mary446@users.noreply.github.com>
2023-07-27 14:17:30 +0300

    completed assignment

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,22 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  const  actor= actors.map((actor)=>(
+    <div>
+   <h1>{actor.name}</h1>
+   <ul>
+       {actor.movies.map((movie)=>(
+         <li key={movie}>{movie}</li>
+       ))}
+   </ul>
+ </div>
+  ));
+ console.log(actor);
+ return <div>
+   <h1>Actors Page</h1>
+   {actor}
+   </div>;
 }
+
 
 export default Actors;
